74c9e12ec03764bf994e957d66f58823cdb0f0e9
KedaloEM <kedaloem@yandex.ru>
2016-02-29 17:58:56 +0300

    Added files via upload

--- pacman.py
+++ pacman.py
@@ -7,6 +7,8 @@ import random
 tile_size = 32
 map_size = 16
 
+'''initialisation of the window'''
+
 
 def init_window():  # Generating the window
     pygame.init()
@@ -14,7 +16,10 @@ def init_window():  # Generating the window
     pygame.display.set_caption('Pacman')
 
 
-def draw_background(scr, img=None):
+'''Function with pygame method Surface, which draws the window'''
+
+
+def draw_background(scr, img=None):  # Drawing the background
     if img:
         scr.blit(img, (0, 0))
     else:
@@ -23,7 +28,11 @@ def draw_background(scr, img=None):
         scr.blit(bg, (0, 0))
 
 
-class GameObject(pygame.sprite.Sprite):
+'''Common class, which is the origin for all object classes (Pacman, Ghosts, State objects)'''
+
+
+class GameObject(
+    pygame.sprite.Sprite):
     def __init__(self, img, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img)
@@ -33,16 +42,19 @@ class GameObject(pygame.sprite.Sprite):
         self.tick = 0
         self.set_coord(x, y)
 
-    def set_coord(self, x, y):
+    def set_coord(self, x, y):  # setting coordinates to objects according to the map file
         self.x = x
         self.y = y
         self.screen_rect = Rect(floor(x) * tile_size, floor(y) * tile_size, tile_size, tile_size)
 
-    def game_tick(self):
+    def game_tick(self):  # Game time
         self.tick += 1
 
-    def draw(self, scr):
+    def draw(self, scr):  # Pygame function, drawing all the objects
         scr.blit(self.image, (self.screen_rect.x, self.screen_rect.y))
+
+
+'''This class refers to the common GameObject class, includes the information about moving of the ghost'''
 
 
 class Ghost(GameObject):
@@ -54,13 +66,13 @@ class Ghost(GameObject):
         self.velocity = 3.5 / 10.0
 
     @property
-    def intellect(self):
+    def intellect(self):  # AI of "clever ghosts"
         x, y = int(self.x), int(self.y)
         if y == int(pacman.y):
             for X in range(x, 16):
                 if isinstance(MAP.map[y][X], Wall):
                     break
-                if X == int(pacman.x):
+                if X == int(pacman.x):  # Checking the pacman coord (ghost sees it) and makes a decision
                     return 1 if not EatBonus.eat_bonus \
                         else 3
             for X in reversed(range(x)):
@@ -83,14 +95,14 @@ class Ghost(GameObject):
                     return 4 if not EatBonus.eat_bonus \
                         else 2
 
-    def game_tick(self):
+    def game_tick(self):  # Movings of the ghosts with time
         super(Ghost, self).game_tick()
 
-        if self.tick % 20 == 0 or self.direction == 0:
+        if self.tick % 20 == 0 or self.direction == 0:  # direction: 1-moving right 2- up 3-left 4- down
             self.direction = random.randint(1, 4)
 
         decision = self.intellect
-        if decision:
+        if decision:  # clever ghosts move according to the def "Intellect and their initial "direction", when ghost meets the wall, it stopps"
             self.direction = decision
 
         if self.direction == 1:
@@ -117,7 +129,8 @@ class Ghost(GameObject):
             if self.y <= 0:
                 self.y = 0
                 self.direction = random.randint(1, 4)
-        if floor(pacman.x) == floor(self.x) and floor(pacman.y) == floor(self.y) and EatBonus.eat_bonus:
+        if floor(pacman.x) == floor(self.x) and floor(pacman.y) == floor(
+                self.y) and EatBonus.eat_bonus:  # Pacman eats ghosts
             Ghost.ghosts.remove(self)
 
             pacman.score += 2
@@ -127,21 +140,22 @@ class Ghost(GameObject):
         self.set_coord(self.x, self.y)
 
 
+'''This type of ghost has no def Intellect, it has much in common with Ghost'''
 
-class StupidGhost (GameObject):
-    stupidghosts = []  # massiv of ghosts
+
+class StupidGhost(GameObject):
+    stupidghosts = []  # massiv of stupidghosts
 
     def __init__(self, x, y):
         GameObject.__init__(self, './resources/ghost.png', x, y)
         self.direction = 0
         self.velocity = 3.5 / 10.0
 
-    def game_tick(self):
+    def game_tick(self):  # The same structure as clever ghosts
         super(StupidGhost, self).game_tick()
 
         if self.tick % 20 == 0 or self.direction == 0:
             self.direction = random.randint(1, 4)
-
 
         if self.direction == 1:
             if not is_wall(floor(self.x + self.velocity), self.y):
@@ -176,6 +190,8 @@ class StupidGhost (GameObject):
             exit()
         self.set_coord(self.x, self.y)
 
+
+'''In addition to GameObject, there is an information about moving and meeting of the pacman with bonuses'''
 
 
 class Pacman(GameObject):
@@ -233,13 +249,13 @@ class Pacman(GameObject):
             self.score += 1
             Food.num -= 1
             if Food.num == 0:
-                print('WINNER Score:', str(pacman.score))
+                print('WINNER Score:', str(pacman.score))  # When there is no food items on the map, the game is done
                 exit()
 
         if isinstance(MAP.map[int(self.y)][int(self.x)], Bonus):
             MAP.map[int(self.y)][int(self.x)] = None
             pacman.velocity = 6.5 / 10.0
-        if isinstance(MAP.map[int(self.y)][int(self.x)], EatBonus) == True:
+        if isinstance(MAP.map[int(self.y)][int(self.x)], EatBonus):
             MAP.map[int(self.y)][int(self.x)] = None
             EatBonus.eat_bonus = 1
         if isinstance(MAP.map[int(self.y)][int(self.x)], EatableWall) == True:
@@ -261,6 +277,9 @@ def tick_ghosts():
         g.game_tick()
     for g in StupidGhost.stupidghosts:
         g.game_tick()
+
+
+'''class of walls, that can be ruined by Pacman'''
 
 
 class EatableWall(GameObject):
@@ -294,7 +313,7 @@ def draw_eatable_walls(screen):
         GameObject.draw(w, screen)
 
 
-def is_wall(x, y):
+def is_wall(x, y):  # checking the existance of the wall
     return isinstance(MAP.map[int(y)][int(x)], Wall)
 
 
@@ -315,11 +334,17 @@ class Bonus(GameObject):
         GameObject.__init__(self, './resources/bonus.png', x, y)
 
 
+'''Checking class, made only for simplification of using the meetings of pacman with bonuses in other defs'''
+
+
 class EatBonus(GameObject):
     eat_bonus = None
 
     def __init__(self, x, y):
         GameObject.__init__(self, './resources/eat.png', x, y)
+
+
+'''this class includes information about all objects of the map in 2-measured array'''
 
 
 class Map:
@@ -361,7 +386,7 @@ class Map:
                     self.map[y][x].draw(screen)  # draw, if smth is in that square x*y
 
 
-def process_events(events, packman):
+def process_events(events, packman):  # Checking the user has clicked the key
     for event in events:
         if (event.type == QUIT) or (event.type == KEYDOWN and event.key == K_ESCAPE):
             sys.exit(0)
